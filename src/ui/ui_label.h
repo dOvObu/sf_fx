0d1f4158e9ef48a28f7a571a9c8d63089f01b7d8
m_permyakov <m.permiakov@mpgames.rocks>
2020-05-09 03:33:48 +0300

    Add explicit default destructor

--- src/ui/ui_label.h
+++ src/ui/ui_label.h
@@ -8,12 +8,17 @@ namespace qw
 	struct UiLabel : public IUiItem
 	{
 		UiLabel(::sf::String text, char const* font);
+		~UiLabel() override = default;
+
 		void SetPosition(::sf::Vector2f const& position) override;
 		::sf::Vector2f GetPosition() override;
+
 		void SetRotation(float angle) override;
 		float GetRotation() override;
+
 		std::vector<IUiItem*>& GetChilds() override;
 		void AddChild(IUiItem* new_ui_item) override;
+
 		void Draw() override;
 	private:
 		::sf::Text _text;
