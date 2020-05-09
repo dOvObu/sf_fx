023e7574d8aa7e16e99904a9cfbaa280991cea16
m_permyakov <m.permiakov@mpgames.rocks>
2020-05-09 03:31:20 +0300

    Add string for debug

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1,16 +1,16 @@
 #include "mouse.h"
 
 
-qw::Event qw::Mouse::OnMouseLeftButtonPressed;
-qw::Event qw::Mouse::OnMouseLeftButtonReleased;
-qw::Event qw::Mouse::OnMouseLeftButtonDown;
+qw::Event qw::Mouse::OnMouseLeftButtonPressed ("OnMouseLeftButtonPressed ");
+qw::Event qw::Mouse::OnMouseLeftButtonReleased("OnMouseLeftButtonReleased");
+qw::Event qw::Mouse::OnMouseLeftButtonDown    ("OnMouseLeftButtonDown    ");
 
-qw::Event qw::Mouse::OnMouseMiddleButtonPressed;
-qw::Event qw::Mouse::OnMouseMiddleButtonReleased;
-qw::Event qw::Mouse::OnMouseMiddleButtonDown;
+qw::Event qw::Mouse::OnMouseMiddleButtonPressed ("OnMouseMiddleButtonPressed ");
+qw::Event qw::Mouse::OnMouseMiddleButtonReleased("OnMouseMiddleButtonReleased");
+qw::Event qw::Mouse::OnMouseMiddleButtonDown    ("OnMouseMiddleButtonDown    ");
 
-qw::Event qw::Mouse::OnMouseRightButtonPressed;
-qw::Event qw::Mouse::OnMouseRightButtonReleased;
-qw::Event qw::Mouse::OnMouseRightButtonDown;
+qw::Event qw::Mouse::OnMouseRightButtonPressed ("OnMouseRightButtonPressed ");
+qw::Event qw::Mouse::OnMouseRightButtonReleased("OnMouseRightButtonReleased");
+qw::Event qw::Mouse::OnMouseRightButtonDown    ("OnMouseRightButtonDown    ");
 
 sf::RenderWindow* qw::Mouse::pw{ nullptr };
